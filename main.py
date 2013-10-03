b32f8b15ed11d3dc008372596508cce529e93b32
jonyrock <ilms@live.ru>
2013-10-03 22:10:34 +0400

    video write test

--- main.py
+++ main.py
@@ -5,9 +5,14 @@ import numpy as np
 
 # global properties
 videoFilePath = 'Cam1_Outdoor.mp4'
+videoOutFilePath = 'out.avi'
 backgroundFinder = AvgBackgroundFinder(videoFilePath)
 backThreshold = 230
 
+#global vars
+videoWidth = None
+videoHeight = None
+videoFps = None
 
 def main():
     print('Start video processing: ' + videoFilePath)
@@ -20,22 +25,36 @@ def main():
 
 def computeMask(back):
     vc = cv2.VideoCapture(videoFilePath)
+    videoWidth = int(vc.get(cv2.cv.CV_CAP_PROP_FRAME_WIDTH))
+    videoHeight = int(vc.get(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT))
+    videoFps = int(vc.get(cv2.cv.CV_CAP_PROP_FPS))
+
     def ifThreshold(x, t):
         if abs(x) > t:
             return 255
         else:
             return 0
+
     diffToMaskMaker = np.vectorize(ifThreshold)
-    cv2.namedWindow('window', 0)
-    while (True):
+    # cv2.namedWindow('window', 0)
+    writer = cv2.VideoWriter(videoOutFilePath, cv2.cv.CV_FOURCC(*'MJPG'), videoFps, 
+                             (videoWidth, videoHeight), False)
+
+    # while (True):
+    for i in range(300):
         isRead, frame = vc.read()
         if not isRead:
             break
         frame = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
         frame = diffToMaskMaker(frame - back, backThreshold).astype(np.uint8)
-        cv2.imshow('window', frame)
-        k = cv2.waitKey()
-        if k == 27:
-            break
+        writer.write(frame)
+        # cv2.imshow('window', frame)
+        # k = cv2.waitKey()
+        # if k == 27:
+        #     break
+
+    writer.release()
+
 
 main()
+cv2.destroyAllWindows()
--- snippents.py
+++ snippents.py
@@ -1,15 +1,36 @@
+import cv2, numpy as np
 
-import cv2
 
 def imageShow(image):
     cv2.namedWindow('window', 0)
     cv2.imshow('window', image)
     cv2.waitKey()
 
+
 def imageShowTest():
     image = cv2.imread('testImg.jpg')
     imageShow(image)
-    
+
+
 def imageAccessTest():
     image = cv2.imread('testImg.jpg')
     print image[:, 1:3]
+
+
+def testWrite():
+    img1 = cv2.imread('test1.jpg')
+    img2 = cv2.imread('test1.jpg')
+    img3 = cv2.imread('test1.jpg')
+    
+    height , width , layers =  img1.shape
+    
+    video = cv2.VideoWriter('video.avi', cv2.cv.CV_FOURCC(*'MJPG'), 1,(width,height))
+    
+    video.write(img1)
+    video.write(img2)
+    video.write(img3)
+    
+    cv2.destroyAllWindows()
+    video.release()
+    
+# testWrite()
