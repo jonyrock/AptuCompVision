5b06b59d603d5e2b6fe6651943b69a0a2f22e2cd
jonyrock <ilms@live.ru>
2013-10-04 00:23:08 +0400

    without rgb again (uing bug fix)

--- backgroundFinder/avgBackgroundFinder.py
+++ backgroundFinder/avgBackgroundFinder.py
@@ -8,14 +8,14 @@ class AvgBackgroundFinder(BackgroundFinder):
         self.sumArr += frame
 
     def computeBack(self):
-        self.sumArr = np.ndarray(shape=(self.height, self.width, 3), dtype=np.long) * 0
+        self.sumArr = np.ndarray(shape=(self.height, self.width), dtype=np.long) * 0
         frameCount = 0
         while True:
             isRead, frame = self.vc.read()
             if not isRead:
                 break
             frameCount += 1
-            # frame = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
+            frame = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
             self.addFrame(frame)
         if frameCount == 0:
             raise Exception("Can't read video")
--- main.py
+++ main.py
@@ -5,12 +5,12 @@ import numpy as np
 import tools
 
 # global properties
-# videoFilePath = 'Cam1_Outdoor.mp4'
-videoFilePath = 'car-overhead-1.avi'
+videoFilePath = 'Cam1_Outdoor.mp4'
+# videoFilePath = 'car-overhead-1.avi'
 
 videoOutFilePath = 'out.avi'
 backgroundFinder = AvgBackgroundFinder(videoFilePath)
-backThreshold = 200
+backThreshold = 7
 
 #global vars
 videoWidth = None
@@ -48,13 +48,13 @@ def computeMask(back):
     for i in range(900):
         vc.read()
         
-    for i in range(6):
+    for i in range(900):
         isRead, frame = vc.read()
         if not isRead:
             break
-        # frame = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
-        frame -= back
-        frame = tools.map3dTo2d(frame, lambda r,g,b: abs(r) + abs(g) + abs(b))
+        frame = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY).astype(int)
+        frame -= back.astype(int)
+        # frame = tools.map3dTo2d(frame, lambda r,g,b: abs(r) + abs(g) + abs(b))
         frame = diffToMaskMaker(frame, backThreshold).astype(np.uint8)
         
         # if(i > 1500):
@@ -64,9 +64,9 @@ def computeMask(back):
         # if i == 4500:
         #     kernel = cv2.getStructuringElement(1, (6, 6))
         
-        cv2.imwrite('img' + str(i) + '.jpg', frame)
+        # cv2.imwrite('img' + str(i) + '.jpg', frame)
         
-        # writer.write(frame)
+        writer.write(frame)
         # cv2.imshow('window', frame)
         # k = cv2.waitKey()
         # if k == 27:
