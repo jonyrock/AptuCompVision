f88a39a8ea2a1fe68bb53aedeb848ed3a7e2229f
jonyrock <ilms@live.ru>
2013-10-24 19:43:49 +0400

    #2 imp

--- task2_filters/main.py
+++ task2_filters/main.py
@@ -1,6 +1,15 @@
-
 import numpy as np
 import cv2
 
-for i in range(0, 100):
-    print(i)
+IMAGE_NOISY_PATH = 'resources/noisy.png'
+IMAGE_ORIGINAL_PATH = 'resources/original.png'
+
+img = cv2.imread(IMAGE_NOISY_PATH)
+if (img is None):
+    print("Can't read image '" + IMAGE_NOISY_PATH + "'")
+    exit(1)
+img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+
+cv2.imwrite('out.png', img)
+
+    
