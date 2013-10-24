3ef9901f7cfdf0400662f98cbfee0f186bdde888
jonyrock <ilms@live.ru>
2013-10-24 22:10:34 +0400

    #6 imp

--- task2_filters/main.py
+++ task2_filters/main.py
@@ -6,6 +6,8 @@ IMAGE_NOISY_PATH = 'resources/noisy.png'
 IMAGE_ORIGINAL_PATH = 'resources/original.png'
 
 OUT_GAUSSIAN_PATH = 'out/gaussian.png'
+OUT_BILATERIAL_PATH = 'out/bilaterial.png'
+OUT_BILATERIAL_PATH2 = 'out/bilaterial2.png'
 
 
 img = cv2.imread(IMAGE_NOISY_PATH)
@@ -26,20 +28,27 @@ def noiseDiff(otherImage):
     diffSum = 0
     for i in range(0, otherImage.shape[0]):
         for j in range(0, otherImage.shape[1]):
-            diffSum += max(img[i, j], imgOriginal[i, j]) - min(img[i, j], imgOriginal[i, j])
+            diffSum += max(otherImage[i, j], imgOriginal[i, j]) - min(otherImage[i, j], imgOriginal[i, j])
     return diffSum / (otherImage.shape[0] * otherImage.shape[1] * 1.0)
 
 def gaussianFilter():
     return cv2.GaussianBlur(img, (5,5), 5)
 
+def bilaterialFilter():
+    return cv2.bilateralFilter(img, 5, 150, 200)
 
 # output
 if not os.path.exists('out'):
     os.mkdir('out')
+
 imgGaussian = gaussianFilter()
-
 print("Gaussian noise cost: " + str(noiseDiff(imgGaussian)))
-
 cv2.imwrite(OUT_GAUSSIAN_PATH, imgGaussian)
+
+imgBilaterial = bilaterialFilter()
+print("Bilaterial noise cost: " + str(noiseDiff(imgBilaterial)))
+cv2.imwrite(OUT_BILATERIAL_PATH, imgBilaterial)
+
+
 
     
