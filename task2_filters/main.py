c590d4942b1ed8b1bc0391ced1cd16855ff91ae6
jonyrock <ilms@live.ru>
2013-10-24 21:32:18 +0400

    # imp

--- task2_filters/main.py
+++ task2_filters/main.py
@@ -1,8 +1,12 @@
 import numpy as np
 import cv2
+import os
 
 IMAGE_NOISY_PATH = 'resources/noisy.png'
 IMAGE_ORIGINAL_PATH = 'resources/original.png'
+
+OUT_GAUSSIAN_PATH = 'out/gaussian.png'
+
 
 img = cv2.imread(IMAGE_NOISY_PATH)
 if (img is None):
@@ -10,6 +14,15 @@ if (img is None):
     exit(1)
 img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
 
-cv2.imwrite('out.png', img)
+
+def gaussianFilter():
+    return cv2.GaussianBlur(img, (5,5), 5)
+
+
+# output
+if not os.path.exists('out'):
+    os.mkdir('out')
+imgGaussian = gaussianFilter()
+cv2.imwrite(OUT_GAUSSIAN_PATH, imgGaussian)
 
     
