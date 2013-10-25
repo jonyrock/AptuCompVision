71ca9ea4b94179b19c3d9a035e79679e44668e9d
jonyrock <ilms@live.ru>
2013-10-25 15:32:20 +0400

    #13 imp

--- task3_dots/main.py
+++ task3_dots/main.py
@@ -12,3 +12,24 @@ imgB = cv2.imread(IMAGE_TRANSFORMED_PATH)
 # output
 if not os.path.exists('out'):
     os.mkdir('out')
+    
+    
+import cv2
+import numpy as np
+
+filename = IMAGE_ORIGINAL_PATH
+img = cv2.imread(filename)
+gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+
+gray = np.float32(gray)
+dst = cv2.cornerHarris(gray,2,3,0.04)
+
+#result is dilated for marking the corners, not important
+dst = cv2.dilate(dst,None)
+
+# Threshold for an optimal value, it may vary depending on the image.
+img[dst>0.1*dst.max()]=[0,0,255]
+
+cv2.imshow('dst',img)
+if cv2.waitKey(0) & 0xff == 27:
+    cv2.destroyAllWindows()
