b17481be41fdf284634606de01987fb615a4207f
jonyrock <ilms@live.ru>
2013-10-25 15:57:39 +0400

    #15 begin

--- task3_dots/main.py
+++ task3_dots/main.py
@@ -10,11 +10,18 @@ imgAGray = cv2.cvtColor(imgA,cv2.COLOR_BGR2GRAY)
 imgB = cv2.imread(IMAGE_TRANSFORMED_PATH)
 imgBGray = cv2.cvtColor(imgB,cv2.COLOR_BGR2GRAY)
 
-dst = cv2.cornerHarris(imgAGray,3,3,0.04)
-dst = cv2.dilate(dst,None)
+dstA = cv2.cornerHarris(imgAGray,3,3,0.04)
+dstA = cv2.dilate(dstA,None)
+
+dstB = cv2.cornerHarris(imgBGray,3,3,0.04)
+dstB = cv2.dilate(dstB,None)
 
 # Threshold for an optimal value, it may vary depending on the image.
-imgA[dst>0.1*dst.max()]=[0,0,255]
+imgA[dstA>0.1*dstA.max()]=[0,0,255]
+imgB[dstB>0.1*dstB.max()]=[0,0,255]
+
+img = np.concatenate((imgA, imgB), axis=1)
+
 
 
 cv2.imshow('dsttt',img)
